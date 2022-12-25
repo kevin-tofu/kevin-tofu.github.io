9f8803e95a78c2a38b4e91f9832857138486f541
kohei <koheitech001@gmail.com>
2022-12-25 13:06:38 +0900

    up

---
+++ assets/axios.0d99fdfa.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.43ae9f17.js";import{a}from"./axios.30238dbd.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
