55717c6663cf2d7d4a0388ed8e1a7eaea5792a55
kohei <koheitech001@gmail.com>
2023-01-10 18:20:36 +0900

    up

--- assets/GarallyPage.084e3728.js
+++
@@ -1,1 +0,0 @@
-import"./axios.243292df.js";import{_ as e,H as o,I as t,Q as a}from"./index.859cb193.js";const n=o({name:"GarallyPage",components:{},props:{},setup(){return{}}}),r={class:"about"};function s(c,p,_,i,l,d){return t(),a("div",r," Under Construction ")}var f=e(n,[["render",s]]);export{f as default};
