fe83e08677a58d6b04f94ed798ce69c9e2336ceb
kohei <koheitech001@gmail.com>
2023-08-16 12:34:53 +0900

    up

--- assets/axios.b2611535.js
+++
@@ -1,1 +0,0 @@
-import{f as i}from"./index.5697c8e9.js";import{a}from"./index.fce87546.js";import"./axios.243292df.js";const r=a.create({baseURL:"https://api.example.com"});var p=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=r});export{r as api,p as default};
--- assets/index.fce87546.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./axios.243292df.js";var o=a.exports,s=o;export{s as a};
