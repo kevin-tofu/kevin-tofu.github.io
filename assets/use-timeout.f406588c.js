e30ef9059ef901d64a17d62508fc6f89db6f7f71
kohei <koheitech001@gmail.com>
2024-10-01 22:23:27 +0900

    fix search button

---
+++ assets/use-timeout.f406588c.js
@@ -0,0 +1,1 @@
+import{b as u}from"./vm.b6397151.js";import{a1 as i,l as m,g as s}from"./index.1b94304b.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
